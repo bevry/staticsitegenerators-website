cfc0d8527e6d3c7c0069747f58f4dcde829845f2
Benjamin Lupton <b@lupton.cc>
2018-12-18 21:29:12 +1100

    got it working

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
-// https://www.npmjs.com/package/@zeit/next-typescript
-const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript()
+// https://spectrum.chat/zeit/general/unable-to-import-module-now-launcher-error~2662f0ba-4186-402f-b1db-2e3c43d8689a?m=MTU0NTEyNjk4ODE3Mw==
+const env =
+	process.env.NODE_ENV === 'development'
+		? {} // We're never in "production server" phase when in development mode
+		: !process.env.NOW_REGION
+		? require('next/constants') // Get values from `next` package when building locally
+		: require('next-server/constants') // Get values from `next-server` package when building on now v2
+
+module.exports = (phase, { defaultConfig }) => {
+	if (phase === env.PHASE_PRODUCTION_SERVER) {
+		// Config used to run in production.
+		return {}
+	}
+
+	const withTypescript = require('@zeit/next-typescript')
+	return withTypescript()
+}
