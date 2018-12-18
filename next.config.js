70a90381d3d395e49587827f085ee84ff118989b
Benjamin Lupton <b@lupton.cc>
2018-12-18 21:16:32 +1100

    attempt at a deploy that uses the exact next.config.js file that is recommended

--- next.config.js
+++ next.config.js
@@ -1,2 +1,3 @@
+// https://www.npmjs.com/package/@zeit/next-typescript
 const withTypescript = require('@zeit/next-typescript')
-module.exports = withTypescript({})
+module.exports = withTypescript()
